9cd3e0fa025de2bc469193f15f6110520b343bcf
Kristian Arnesen Vik <kristianarnesenvik@gmail.com>
2020-12-26 17:24:18 +0100

    add removal and arrow key support

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,43 @@
 const el = document.querySelector("#pic_container");
 
-var scrollCount = 1;
-var picCount = 0;
+//var scrollCount = 1; // delete this
+var picCount = 1;
 var lastSpawned;
 
-function scrollEvent(event) { // bad event-listener, change in future.
-  scrollCount++;
-  if (scrollCount % 5 == 0) {
-    picCount++;
-    spawnImage(picCount);
-  }
+// event-listeners:
+
+function callToSpawn(){
+  spawnImage(picCount);
 }
 
+function callToRemove(){
+  removeImage(lastSpawned);
+}
+
+// Event-listeners for showing new image
+document.getElementById("pic_container").addEventListener('click', callToSpawn);
+
+// control feed with arrow-keys
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            callToRemove();
+            break;
+        case 38:
+            callToRemove();
+            break;
+        case 39:
+            callToSpawn();
+            break;
+        case 40:
+            callToSpawn();
+            break;
+    }
+};
+
+
 function spawnImage(picNumber) { // shows next image
-  console.log("Spawner bilde: " + picNumber);
+  console.log("Spawning image: " + picNumber);
   // creates random numbers for positioning of the image within the parent div.
   var randomML = (Math.random() * 58);
   var randomMR = (Math.random() * 58);
@@ -28,16 +52,43 @@ function spawnImage(picNumber) { // shows next image
   img.classList.add("innerimage");
   img.id = String("picnr" + picNumber);
   src.appendChild(img);
-  lastSpawned = String("picnr" + (picNumber - 1)); //adds this picture to the lastSpawned variable for future blurring
+  lastSpawned = (picNumber - 1); //adds last picture to lastSpawned variable for blurring or removing.
   blurlast();
+  picCount++;
+  console.log(picCount);
+}
+
+function removeImage(picNumber) {
+  var src = document.querySelector("#pic_container");
+  var ele = document.getElementById(String("picnr" + (lastSpawned+1)));
+  try {
+      src.removeChild(ele);
+      lastSpawned--;
+      picCount--;
+      console.log("Removing image " + ele)
+      unblurWhenRemove();
+  }
+  catch {
+    console.log("No image to remove");
+  }
 }
 
 function blurlast(){
-  var ele = document.getElementById(String(lastSpawned));
+  var ele = document.getElementById(String("picnr" + lastSpawned));
   try {
-        ele.style.filter = "blur(3px) grayscale(0)";
+        ele.style.filter = "blur(0px) grayscale(100)";
   }
-  catch {
-    console.log("No pic to blur.");
+ catch {
+    console.log("No image to blur.");
+  }
+}
+
+function unblurWhenRemove(){
+  var ele = document.getElementById(String("picnr" + (lastSpawned + 1)));
+  try {
+        ele.style.filter = "blur(0px) grayscale(0)";
+  }
+ catch {
+    console.log("No image to unblur.");
   }
 }
